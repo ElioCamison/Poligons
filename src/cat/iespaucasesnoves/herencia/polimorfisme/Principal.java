a312b18903a7bcfcf4292f538c149856da24f5a9
Elio <Elio@LAPTOP-R2J5CDRF>
2016-04-13 19:31:21 +0200

    Comit inicial

--- src/cat/iespaucasesnoves/herencia/polimorfisme/Principal.java
+++ src/cat/iespaucasesnoves/herencia/polimorfisme/Principal.java
@@ -5,6 +5,7 @@
  */
 package cat.iespaucasesnoves.herencia.polimorfisme;
 
+import cat.iespaucasesnoves.herencia.polimorfisme.familia.Fill;
 import cat.iespaucasesnoves.herencia.polimorfisme.familia.Net;
 import cat.iespaucasesnoves.herencia.polimorfisme.familia.Pare;
 /**
@@ -13,18 +14,30 @@ import cat.iespaucasesnoves.herencia.polimorfisme.familia.Pare;
  */
 public class Principal {
     
+    public void mostrarComu (Pare object){
+        System.out.println(object.getClass());
+    }
+    
     public void objectePare (Pare object){
 //        System.out.println("BLALBLALBLA");
 //        System.out.println(object.getClass());
         System.out.println((object instanceof Net));
     }
     
+    public static void mostrarEstaticPare (Pare object){
+        
+    }
+    
+    
     
     public static void main(String[] args){
         Net net1 = new Net();
+        Pare pare1 = new Pare();
+        Fill fill1 = new Fill();
         Principal o = new Principal();
         o.objectePare(net1);
-        System.out.println("Hola, això és una prova de git :D");
-        
+        o.mostrarComu(net1);
+        o.mostrarComu(pare1);
+        o.mostrarComu(fill1);
     }
 }
